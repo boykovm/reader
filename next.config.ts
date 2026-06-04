7e217bb642013503a3d3a7574846be5452efcd06
vadimboyko <vadim.boyko.01@outlook.com>
2026-06-04 16:06:04 +0300

    next config rollback

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export',          // Outputs static HTML/CSS/JS to an 'out' folder
-    images: {
-        unoptimized: true,       // GitHub Pages won't support Next.js default image optimization
-    },
-    basePath: '/reader',
 };
 
 export default nextConfig;
