7874853b717482c62fc24ee1f1cae13c598d6611
vadimboyko <vadim.boyko.01@outlook.com>
2026-06-04 13:02:08 +0300

    deploy test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',          // Outputs static HTML/CSS/JS to an 'out' folder
+    images: {
+        unoptimized: true,       // GitHub Pages won't support Next.js default image optimization
+    },
+    basePath: '/reader',
 };
 
 export default nextConfig;
